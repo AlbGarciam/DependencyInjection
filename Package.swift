9ef84c39d0bba950f6a2aaa787d852c738e9f5ce
Alberto Garcia <alb.garciam@gmail.com>
2021-12-18 16:28:53 +0100

    Update platforms

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "DependencyInjection",
+    platforms: [.iOS("11.0"), .macOS("10.10"), .tvOS("11.0")],
     products: [
         .library(name: "DependencyInjectionStatic", type: .static, targets: ["DependencyInjection"]),
         .library(name: "DependencyInjection", type: .dynamic, targets: ["DependencyInjection"]),
