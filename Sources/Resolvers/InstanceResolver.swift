1e9dab1a4f676d4dbfe9d1e7686cec128e4c5ff8
AlbGarciam <alb.garciam@gmail.com>
2023-01-16 09:17:12 +0100

    Fix concurrency issues

--- Sources/Resolvers/InstanceResolver.swift
+++ Sources/Resolvers/InstanceResolver.swift
@@ -8,23 +8,30 @@
 import Foundation
 
 struct InstanceResolver: ResolverContract {
+    private static let queue = DispatchQueue(label: "instance-resolver", attributes: .concurrent)
     private static var single: [String: [Injectable.Type]] = [:]
 
     static func register<T: Injectable>(_ type: Any, _ implementation: T.Type) {
         guard let key = try? getMapKeyFor(type) else {
             return
         }
-        var classes = single[key] ?? []
-        classes.append(implementation)
-        single[key] = classes
+        queue.sync(flags: .barrier) {
+            var classes = single[key] ?? []
+            classes.append(implementation)
+            single[key] = classes
+        }
     }
 
     static func resolve<T>(_ type: T.Type) throws -> T! {
         let key = try getMapKeyFor(T.self)
-        if let contracts = single[key] {
+        if let contracts = getRegisteredTypes(key) {
             return try resolveBestMatch(contracts)
         }
         return nil
+    }
+    
+    private static func getRegisteredTypes(_ key: String) -> [Injectable.Type]? {
+        queue.sync { single[key] }
     }
 
     static func reset() {
