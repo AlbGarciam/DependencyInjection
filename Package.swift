123f0cb69884f5b7397b301e2f0058d31b732141
AlbGarciam <alb.garciam@gmail.com>
2024-02-10 10:53:47 +0100

    Add support for vision OS

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "DependencyInjection",
-    platforms: [.iOS("11.0"), .macOS("10.10"), .tvOS("11.0"), .watchOS("8.0")],
+    platforms: [.iOS("11.0"), .macOS("10.10"), .tvOS("11.0"), .watchOS("8.0"), .visionOS("1.0")],
     products: [
         .library(name: "DependencyInjectionStatic", type: .static, targets: ["DependencyInjection"]),
         .library(name: "DependencyInjection", type: .dynamic, targets: ["DependencyInjection"]),
